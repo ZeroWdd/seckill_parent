d30e5c257aa6cc5dbdc561f7a146c7bebff1bde1
ZeroWdd <1710031565@qq.com>
2019-09-14 10:07:35 +0800

    zuul网关

---
+++ seckill_zuul/src/main/java/com/seckill/zuul/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.seckill.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Auther: wdd
+ * @Date: 2019/9/14 09:51
+ * @Description:
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
